1649d9adf3ad4e949561ffc123470cd68b16e235
David Taccetta <taccetta14@gmail.com>
2022-06-10 12:20:21 -0400

    Initial CarClass Project Commit

---
+++ CarClassProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarClassProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCurrentCar = new Car() { Make = "Kia", Modle = "Rio", Year = 2021};
+            Console.WriteLine($"I drive a {myCurrentCar.Year} {myCurrentCar.Make} {myCurrentCar.Modle}.");
+            Console.WriteLine();
+
+            var myLastCar = new Car() { Make = "Volkswagen", Modle = "Jetta", Year = 2017 };
+            Console.WriteLine($"I used to drive a {myLastCar.Year} {myLastCar.Make} {myLastCar.Modle}.");
+        }
+    }
+}
